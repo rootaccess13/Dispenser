054abde51ce0eed3d005fc09ee96533e29a03b3a
rootaccess13 <43539755+rootaccess13@users.noreply.github.com>
2024-09-05 16:14:03 +0800

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api/device-data/', get_device_data, name='get_device_data'),
     path('api/count-devices/', get_total_gallon, name='count_devices'),
     path('api/devices/', DevicesView.as_view(), name='devices'),
-    path('api/device-list/', DeviceListView.as_view(), name='device-list')
+    path('api/device-list/', DeviceListView.as_view(), name='device-list'),
+    path('api/device-list/<str:device_id>/', DeviceListView.as_view(), name='device-list-status'),
 ]
