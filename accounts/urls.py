ece0e808c65fc79ee5e130a84bed8445e315b791
rootaccess13 <43539755+rootaccess13@users.noreply.github.com>
2024-09-30 02:45:11 +0800

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,10 @@ from .views import RegisterView, CustomLoginView, CustomLogoutView, CustomPasswo
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-verification/', EmailVerificationView.as_view(), name='email_verification'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+
 ]
