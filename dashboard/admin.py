1f7ab42b019a2d79ea784ab0322bc3ff046d9e3e
rootaccess13 <43539755+rootaccess13@users.noreply.github.com>
2024-11-25 23:11:22 +0800

    Update admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Devices, DeviceList
+from .models import Devices, DeviceList, Gallon
 
 @admin.register(Devices)
 class DevicesAdmin(admin.ModelAdmin):
@@ -12,3 +12,10 @@ class DevicesAdmin(admin.ModelAdmin):
 class DeviceListAdmin(admin.ModelAdmin):
     list_display = ('deviceId', 'status', 'device_location', 'time_on', 'time_off','is_consumed', 'date_added')
     list_filter = ('status',)
+
+class GallonAdmin(admin.ModelAdmin):
+    list_display = ('date_added', 'total_quantity', 'gallons_added', 'gallons_deleted', 'gallons_consumed', 'gallons_remaining')
+    search_fields = ('date_added',)
+
+admin.site.register(Gallon, GallonAdmin)
+admin.site.site_header = 'Dispencer Admin'
